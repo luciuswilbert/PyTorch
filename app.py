2e1ecb529d1dbe315d705939cb91f205bff8b450
luciuswilbert <luciuswilbert@gmail.com>
2025-12-25 20:43:22 +0800

    Tensors

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import torch
 
-tensors = torch.arange(0, 100, 10)
-tensors = tensors.type(torch.float32)
-
-print(tensors.min())
-print(tensors.max())
-print(tensors.mean())
-print(tensors.sum())
+tensors = torch.arange(1., 8.)
+reshaped_tensors = tensors.reshape(1, 7)
+print(reshaped_tensors)
