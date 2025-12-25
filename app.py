727a9c666ddd402b8ad3ed34596fe9b9be8f476e
luciuswilbert <luciuswilbert@gmail.com>
2025-12-25 20:33:42 +0800

    Tensors

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import torch
 
-print(torch.__version__)
+tensors = torch.arange(0, 100, 10)
+tensors = tensors.type(torch.float32)
+
+print(tensors.min())
+print(tensors.max())
+print(tensors.mean())
+print(tensors.sum())
