7357e8458a71b16b05ec0b4153511c310a452940
luciuswilbert <luciuswilbert@gmail.com>
2025-12-26 08:37:27 +0800

    Tensors

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 import torch
 
-RANDOM_SEED = 42
-torch.manual_seed(seed=RANDOM_SEED)
-random_tensor_c = torch.rand(3, 4)
-torch.manual_seed(seed=RANDOM_SEED)
-random_tensor_d = torch.rand(3, 4)
-print(random_tensor_c)
-print(random_tensor_d)
+tensors = torch.rand(7, 7)
+print(tensors.shape)
